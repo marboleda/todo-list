da6bbf8f3c4039465a75393291a367232aa15b33
Marco Arboleda <arboleda.marco@gmail.com>
2020-06-16 18:48:39 -0400

    Make default project's due date 1 month from today

--- src/initial-page-load.js
+++ src/initial-page-load.js
@@ -1,12 +1,12 @@
-import { createNewProject, addNewToDo } from './project'
-import { format, endOfToday } from 'date-fns'
+import { createNewProject } from './project'
+import { format, endOfToday, addMonths } from 'date-fns'
 
 const initialPageLoad = () => {
     //Should only generate default project if no projects exist yet, but will implement that check later
     //For now, just create the default project
     const defaultProject = createNewProject('Default Project', 
                                             "Your first project!", 
-                                            format(endOfToday(), 'MM/dd/yyyy hh:mm a'), 
+                                            format(addMonths(endOfToday(), 6), 'MM/dd/yyyy hh:mm a'), 
                                             0, 
                                             []);
 
