06a719d3b6f3f0290d5a7672e4fe51c3dab0a40a
Marco Arboleda <arboleda.marco@gmail.com>
2020-06-16 18:21:02 -0400

    Incorporate date-fns library

--- src/initial-page-load.js
+++ src/initial-page-load.js
@@ -1,9 +1,14 @@
-import {createNewProject} from './todo'
+import { createNewProject } from './todo'
+import { format, endOfToday } from 'date-fns'
 
 const initialPageLoad = () => {
     //Should only generate default project if no projects exist yet, but will implement that check later
     //For now, just create the default project
-    const defaultProject = createNewProject('Default Project', "Your first project!", "06/19/2020", 0, {});
+    const defaultProject = createNewProject('Default Project', 
+                                            "Your first project!", 
+                                            format(endOfToday(), 'MM/dd/yyyy hh:mm a'), 
+                                            0, 
+                                            {});
 }
 
 export default initialPageLoad;
